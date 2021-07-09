f94c4cf9816a3b1c357705f66a7a029cc8008a78
bhyou <bhyou@foxmail.com>
2021-07-09 14:29:49 +0800

    abstracted for pixel cell

--- bench/pixelCell.sv
+++ bench/pixelCell.sv
@@ -5,48 +5,48 @@
  > Mail        : bhyou@foxmail.com 
  > Created Time: Wed 07 Jul 2021 05:07:01 PM CST
  ************************************************************************/
- 
+`include "analog_front_end.sv"
+
 module pixelCell(
     output real          collectEnergyL,
     input  real          collectEnergyS,
     input  real          collectEnergyE,
     input  real          collectEnergySE,
-    output logic         discOutLocal  ,
+    output wire          discOutLocal  ,
 
     input  wire          SummingMode   ,
     input  wire   [3:0]  discOutNear   ,
     output wire   [3:0]  ackToNear     ,
-
     input  wire   [3:0]  ackFromNear   ,
-//    output wire          hitPulse    ,
 
+    output wire          discOutSumLocal,
     input  wire   [2:0]  discOutSumNear,
-//    output wire          sumPulse    ,
-
+    input  wire          refclk  ,
     input  wire          clk_read ,
     input  wire          reset    ,
     input  wire          shutter  ,
     input  wire          SerInA   ,
     input  wire          SerInB   ,
     output wire          SerOutA  ,
-    output wire          SerOutB 
+    output wire          SerOutB  ,
 );
-
     parameter CoorX = 25;
     parameter CoorY = 25;
-
-    mailbox              receivePhotons;
-    sensor               sernsor;
+    sensor_inf sensorInf (refclk);
+    mailbox              mbx;
+    analog_front_end     aFE;
 
     initial begin
         photon      photon;
-        sensor = new(CoorY,CoorY,receivePhotons);
+        aFE = new(CoorX,CoorY,mbx,sensorInf);
+
         forever begin
-            receivePhotons.get(photon);
-            sensor.convert_energy_to_voltage(collectEnergyL);
+            aFE.hit_reaction(collectEnergyS,collectEnergyE,collectEnergySE,collectEnergyL);
         end
     end
-    
+
+    assign discOutLocal    = sensorInf.discOutLocal;
+    assign discOutSumLocal = sensorInf. discOutSumLocal;     
 
     digit_front_end u_digFrontEnd(
         .SummingMode         (SummingMode    ),
@@ -54,10 +54,8 @@ module pixelCell(
         .ackToNeighbour      (ackToNear      ),
         .discOutLocal        (discOutLocal   ),
         .ackFromNeighbour    (ackFromNear    ),
-        .hitPulse            (               ),
         .discOutSumLocal     (discOutSumLocal),
         .discOutSumNeighbour (discOutSumNear ),
-        .sumPulse            (               ),
         .clk_read            (clk_read       ),
         .reset               (reset          ),
         .shutterA            (shutter        ),
@@ -67,8 +65,5 @@ module pixelCell(
         .SerOutA             (SerOutA        ),
         .SerOutB             (SerOutB        ) 
     );
-
-
-
 
 endmodule
